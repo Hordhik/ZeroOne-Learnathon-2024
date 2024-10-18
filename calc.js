9c4301ba45495ce64891125dc1fae671141de82b
konda Hordhik Manikant Reddy <2300031095@kluniversity.in>
2024-10-18 13:05:16 +0530

    2300031095

---
+++ calc.js
@@ -0,0 +1,21 @@
+function add(a,b){
+  return a+b;
+}
+
+function sub(a,b){
+  return a-b;
+}
+function mul(a,b){
+  return a*b;
+}
+function div(a,b){
+  return a/b;
+}
+
+let a =10;
+let b=20;
+
+console.log(add(b,a));
+console.log(sub(b,a));
+console.log(mul(b,a));
+console.log(div(b,a));
---
+++ info.js
@@ -0,0 +1,4 @@
+const name = "Manikant";
+const id = "2300031095";
+
+console.log(name,id);
